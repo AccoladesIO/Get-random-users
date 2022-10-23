37015a83511be5df050156320331b3c9b941fa7c
unknown <ononobip@gmail.com>
2022-10-23 16:46:23 +0100

    added pagination

---
+++ src/Pages/Users/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Pagination = ({totalPost, postPerPage, setCurrentPage, currentPage}) => {
+    // get arrays of page numbers
+    let pages = []
+    
+    // work dynamically 
+    for (let i = 1; i<= Math.ceil(totalPost/postPerPage); i++){
+        pages.push(i)
+    }
+  return (
+    <div className='btn-wrapper'>
+        <button onClick={() => setCurrentPage(currentPage - 1)}> prev</button>    
+        {pages.map((page, index) => {
+
+            const switchPage = () => {
+                setCurrentPage(page)
+            }
+            const style = page === currentPage ? 'pagination-btn active' : 'pagination-btn'
+            return (
+                <>
+            <button key={index} className={style} onClick={switchPage} type='button'>{page}</button>
+                </>
+            )
+        })}
+        <button onClick={() => setCurrentPage(currentPage + 1)}> next</button>    
+    </div>
+  )
+}
+
+export default Pagination
