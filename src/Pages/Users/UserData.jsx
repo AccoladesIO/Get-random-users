3cde9d5e11a6512c6a5d1cc70dd5a8140575cba9
unknown <ononobip@gmail.com>
2022-10-27 09:46:36 +0100

    ref and bug fix

--- src/Pages/Users/UserData.jsx
+++ src/Pages/Users/UserData.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
+import React from "react";
+import Pagination from './Pagination'
 
-
-const UserData = ({ data }) => {
+const UserData = ({ data, postPerPage, setCurrentPage, currentPage,  }) => {
   return (
-    <section className='section-user'>
-      <article className='user-info-wrapper'>
-
+    <section className="section-user">
+      <article className="user-info-wrapper">
         {data.map((users, i) => {
-          console.log(users)
+          console.log(users);
           return (
-            <div className='user-info' key={i}>
-              <div className='user-image'>
-                <img src={users.picture.medium} alt='' />
+            <div className="user-info" key={i}>
+              <div className="user-image">
+                <img src={users.picture.medium} alt="" />
               </div>
-              <h3 className='heading-alt'> User {i + 1}</h3>
-              <h3 className='text'>Name: {users.name.first} {users.name.last} </h3>
-              <h3 className='text'>Gender: {users.gender} </h3>
-              <h3 className='text'>Cell: {users.cell}</h3>
+              <h3 className="heading-alt"> User {i + 1}</h3>
+              <h3 className="text">
+                Name: {users.name.first} {users.name.last}{" "}
+              </h3>
+              <h3 className="text">Gender: {users.gender} </h3>
+              <h3 className="text">Cell: {users.cell}</h3>
             </div>
-
-          )
+          );
         })}
+        <Pagination
+        totalPost={data.length}
+        postPerPage={postPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
       </article>
     </section>
+  );
+};
 
-  )
-}
-
-export default UserData
+export default UserData;
