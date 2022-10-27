4bd480cb90a86476b4321d5d9b7c901787b02e2f
unknown <ononobip@gmail.com>
2022-10-27 09:45:40 +0100

    ref and bug fixes

--- src/Pages/Users/Pagination.jsx
+++ src/Pages/Users/Pagination.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 
-const Pagination = ({totalPost, postPerPage, setCurrentPage, currentPage}) => {
+const Pagination = ({ totalPost, postPerPage, setCurrentPage, currentPage }) => {
     // get arrays of page numbers
     let pages = []
-    
+
     // work dynamically 
-    for (let i = 1; i<= Math.ceil(totalPost/postPerPage); i++){
+    for (let i = 1; i <= Math.ceil(totalPost / postPerPage); i++) {
         pages.push(i)
     }
-  return (
-    <div className='btn-wrapper'>
-       {currentPage !== 1 &&  <button onClick={() => setCurrentPage(currentPage - 1)}> prev</button>}    
-        {pages.map((page, index) => {
+    return (
+        <div className='btn-wrapper'>
+            {currentPage !== 1 && <button onClick={() => setCurrentPage(currentPage - 1)}> prev</button>}
+            {pages.map((page, i) => {
 
-            const switchPage = () => {
-                setCurrentPage(page)
-            }
-            const style = page === currentPage ? 'pagination-btn active' : 'pagination-btn'
-            return (
-                <>
-            <button key={index} className={style} onClick={switchPage} type='button'>{page}</button>
-                </>
-            )
-        })}
-        {currentPage !== 10 && <button onClick={() => setCurrentPage(currentPage + 1)}> next</button>}    
-    </div>
-  )
+                const switchPage = () => {
+                    setCurrentPage(page)
+                    // console.log(page)
+                }
+                const style = page === currentPage ? 'pagination-btn active' : 'pagination-btn'
+                return (
+                    <>
+                        <button key={i} className={style} onClick={switchPage} type='button'>{page}</button>
+                    </>
+                )
+            })}
+            {currentPage !== 10 && <button onClick={() => setCurrentPage(currentPage + 1)}> next</button>}
+        </div>
+    )
 }
 
 export default Pagination
