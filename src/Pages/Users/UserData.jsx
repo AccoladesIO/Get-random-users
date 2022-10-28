2b28c9e223a4c75b5eaecfd40678e405b075dd33
unknown <ononobip@gmail.com>
2022-10-28 19:36:10 +0100

    ref

--- src/Pages/Users/UserData.jsx
+++ src/Pages/Users/UserData.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import Pagination from './Pagination'
 
-const UserData = ({ data, postPerPage, setCurrentPage, currentPage,  }) => {
+const UserData = ({ data }) => {
   return (
-    <section className="section-user">
-      <article className="user-info-wrapper">
-        {data.map((users, i) => {
-          console.log(users);
-          return (
-            <div className="user-info" key={i}>
-              <div className="user-image">
-                <img src={users.picture.medium} alt="" />
-              </div>
-              <h3 className="heading-alt"> User {i + 1}</h3>
-              <h3 className="text">
-                Name: {users.name.first} {users.name.last}{" "}
-              </h3>
-              <h3 className="text">Gender: {users.gender} </h3>
-              <h3 className="text">Cell: {users.cell}</h3>
+    <article className="user-info-wrapper">
+      {data.map((users, i) => {
+        console.log(users);
+        return (
+          <div className="user-info" key={i}>
+            <div className="user-image">
+              <img src={users.picture.medium} alt="" />
             </div>
-          );
-        })}
-        <Pagination
-        totalPost={data.length}
-        postPerPage={postPerPage}
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-      />
-      </article>
-    </section>
+            <h3 className="heading-alt"> User {i + 1}</h3>
+            <h3 className="text">
+              Name: {users.name.first} {users.name.last}{" "}
+            </h3>
+            <h3 className="text">Gender: {users.gender} </h3>
+            <h3 className="text">Cell: {users.cell}</h3>
+          </div>
+        );
+      })}
+    </article>
   );
 };
 
